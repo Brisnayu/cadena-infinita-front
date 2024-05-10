e3f5f23a415b055109c19402edab45fdcc2dc3ea
Brisna A. Paez M <124035708+Brisnayu@users.noreply.github.com>
2024-05-10 18:14:02 +0200

    basic settings done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="bg-red-100 p-4">
-      <h1 className="text-2xl font-bold text-gray-800">Hola, mundo!</h1>
+    <div className="bg-purpleDark p-4 font-poppins">
+      <h1 className="text-5xl font-bold text-gray-800">Hola, mundo!</h1>
       <p className="text-gray-600 mt-2">
         Este es un ejemplo de Tailwind CSS en React.
       </p>
