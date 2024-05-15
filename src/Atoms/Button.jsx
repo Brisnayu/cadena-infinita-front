5776a7bb2260dd3669b37ffa41921a7108f6a326
Altus24 <aldu.ing@gmail.com>
2024-05-15 18:12:38 -0300

    atoms/buton fixed

--- src/Atoms/Button.jsx
+++ src/Atoms/Button.jsx
@@ -1,9 +1,6 @@
 function Button({ text, bgColor }) {
-  const bgClass = `bg-${bgColor.replace(/ /g, "-")}`;
-
   return (
-    <button type="submit" 
-    className={`p-2 mt-4 ${bgColor} `}>
+    <button type="submit" className={`p-2 mt-4 ${bgColor} `}>
       {text}
     </button>
   );
